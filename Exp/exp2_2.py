aafbc3de58bfdb4298d162f6beb31df320cee608
JavaBean <javabean6972@outlook.com>
2023-10-25 18:15:50 +0800

    exp2_2 done , to improved

--- Exp/exp2_2.py
+++ Exp/exp2_2.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import cv2
+import exp2_2_demapping
 import math
 
 
@@ -33,21 +34,25 @@ out = np.zeros(shape)
 for i in range(shape[1]):
     for j in range(shape[0]):
         x, y = centralize(i, j, shape[1], shape[0])
-        r = math.sqrt(x ** 2 + y ** 2)
-        if r >= 1:
-            out[j, i] = origin[j, i][::-1]
-            continue
-        theta = (1 - r) ** 2
-
-        real_x = math.cos(theta) * x - math.sin(theta) * y
-        real_y = math.sin(theta) * x + math.cos(theta) * y
+        real_x, real_y = exp2_2_demapping.demapping(x, y)
+        # r = math.sqrt(x ** 2 + y ** 2)
+        # if r >= 1:
+        #     out[j, i] = origin[j, i][::-1]
+        #     continue
+        # theta = (1 - r) ** 2
+        #
+        # real_x = math.cos(theta) * x - math.sin(theta) * y
+        # real_y = math.sin(theta) * x + math.cos(theta) * y
 
         decentralized = decentralize(real_x, real_y, shape[0], shape[1])
         # print((decentralized,i,j,r,shape[0],decentralized[0]))
-        if decentralized[1]>=shape[0] or decentralized[0] >= shape[1]:
+        if decentralized[1] >= shape[0] or decentralized[0] >= shape[1]:
             continue
+        # origin[j, i] = origin[j, i][::-1]
+        # out[int(real_x),int(real_y)] = origin[j, i]
+        # out[int(decentralized[1]), int(decentralized[0])] = origin[j, i]
         out[j, i] = origin[int(decentralized[1]), int(decentralized[0])]
-        out[j, i] = out[j,i][::-1]
+        out[j, i] = out[j, i][::-1]
 
 image = Image.fromarray(out.astype('uint8'))
 save_path = r'E:\MyFiles\KnowledgeBase\Year3Fall\DigitalImageProcessing\Exp\Project\resource\exp2\transformed.png'
---
+++ Exp/exp2_2_demapping.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.optimize import fsolve
+
+
+# 定义映射函数
+def mapping_equation(vars, x_target, y_target):
+    x, y = vars
+    r = np.sqrt(x ** 2 + y ** 2)
+    theta = (1 - r) ** 2
+    if r >= 1:
+        return [x - x_target, y - y_target]
+    else:
+        x_prime = np.cos(theta) * x - np.sin(theta) * y
+        y_prime = np.sin(theta) * x + np.cos(theta) * y
+        return [x_prime - x_target, y_prime - y_target]
+
+
+def demapping(x, y):
+    # 使用fsolve求解非线性方程组
+    initial_guess = [0, 0]  # 初始猜测值
+    solution = fsolve(lambda vars: mapping_equation(vars, x, y), initial_guess)
+    print(solution)
+    return solution
+
+demapping(0.2,0.5)
+demapping(0.3,0.8)
+
